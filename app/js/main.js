36f529df2cf8135a5ce8fcfdf5b43da07e538596
bonchbrubonch <58067420+bonchbrubonch@users.noreply.github.com>
2024-11-28 16:33:59 +0200

    add sections

--- app/js/main.js
+++ app/js/main.js
@@ -5,8 +5,34 @@ $(function () {
 		from: 10000,
 		postfix: " lei"
 	});
-
-
-
-	
 })
+
+
+
+var swiper = new Swiper(".partners__slider", {
+	slidesPerView: 2,
+	grid: {
+		rows: 2,
+	},
+	spaceBetween: 15,
+	pagination: {
+		el: ".swiper-pagination",
+	},
+	breakpoints: {
+		576: {
+			slidesPerView: 4,
+			spaceBetween: 20,
+			grid: {
+				rows: 2,
+			},
+		},
+		992: {
+			slidesPerView: 5,
+			spaceBetween: 30,
+			grid: {
+				rows: 2,
+			},
+		},
+	}
+});
+
